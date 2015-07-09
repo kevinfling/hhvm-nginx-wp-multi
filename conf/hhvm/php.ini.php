4825da28ea79ee3df2c7d0e935a2100b35e366c0
Kevin Fling <k@livebreathedigital.net>
2015-07-09 09:11:12 -0400

    fuck

--- conf/hhvm/php.ini.php
+++ conf/hhvm/php.ini.php
@@ -6,6 +6,8 @@ hhvm.log.level = Warning
 hhvm.log.always_log_unhandled_exceptions = true
 hhvm.log.runtime_error_reporting_level = 8191
 hhvm.mysql.typed_results = false
+hhvm.dynamic_extension_path = /app/.heroku/php/etc/hhvm
+hhvm.dynamic_extensions[pgsql] = pgsql.so
 
 memory_limit = 128M
 
