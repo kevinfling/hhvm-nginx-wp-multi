2644fedb4f43d0b3aa5fee7276526ea6cee69c51
Kevin Fling <k@livebreathedigital.net>
2015-07-09 01:52:34 -0400

    wp rewrite

--- conf/nginx/default_include.conf.php
+++ conf/nginx/default_include.conf.php
@@ -15,6 +15,13 @@ location ~* ^.+\.(ogg|ogv|svg|svgz|eot|otf|woff|mp4|ttf|rss|atom|jpg|jpeg|gif|pn
        access_log off; log_not_found off; expires max;
 }
 
+# Rewrite rules for WordPress Multi-site.
+if (!-e $request_filename) {
+  rewrite /wp-admin$ $scheme://$host$uri/ permanent;
+  rewrite ^/[_0-9a-zA-Z-]+(/wp-.*) $1 last;
+  rewrite ^/[_0-9a-zA-Z-]+(/.*\.php)$ $1 last;
+}
+
 # Uncomment one of the lines below for the appropriate caching plugin (if used).
 #include global/wordpress-wp-super-cache.conf;
 #include global/wordpress-w3-total-cache.conf;
