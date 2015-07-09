ec2a50b0a5f92e4b58522402f4e3d37556170be2
Kevin Fling <k@livebreathedigital.net>
2015-07-09 04:02:49 -0400

    EREROIEHROIH

--- conf/nginx/default_include.conf.php
+++ conf/nginx/default_include.conf.php
@@ -1,29 +1,25 @@
-location / {
-    index  index.php index.html index.htm;
-}
-
 # for people with app root as doc root, restrict access to a few things
 location ~ ^/(composer\.|Procfile$|<?=getenv('COMPOSER_VENDOR_DIR')?>/|<?=getenv('COMPOSER_BIN_DIR')?>/) {
     deny all;
 }
 
+location / {
+  index  index.php;
 
-  location / {
-    # wordpress fancy rewrites
-    if (-f $request_filename) {
-      break;
-    }
-
-    if (-d $request_filename) {
-      break;
-    }
-
-    rewrite         ^(.+)$ /index.php?q=$1 last;
-
+  # wordpress fancy rewrites
+  if (-f $request_filename) {
+    break;
   }
 
-  #location ~ .*\.php$ {
-  #  include         /app/vendor/nginx/conf/fastcgi_params;
-  #  fastcgi_pass    unix:/tmp/php-fpm.socket;
-  #  fastcgi_param   SCRIPT_FILENAME $document_root$fastcgi_script_name;
-  #}
+  if (-d $request_filename) {
+    break;
+  }
+
+  rewrite ^(.+)$ /index.php?q=$1 last;
+}
+
+#location ~ .*\.php$ {
+#  include         /app/vendor/nginx/conf/fastcgi_params;
+#  fastcgi_pass    unix:/tmp/php-fpm.socket;
+#  fastcgi_param   SCRIPT_FILENAME $document_root$fastcgi_script_name;
+#}
