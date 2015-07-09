c09dd569c7ed96832ad983a0ea63a1d6c721c1ea
Kevin Fling <k@livebreathedigital.net>
2015-07-09 03:02:18 -0400

    FUCK

--- conf/nginx/default_include.conf.php
+++ conf/nginx/default_include.conf.php
@@ -1,36 +1,29 @@
+  location / {
+    # wordpress fancy rewrites
+    if (-f $request_filename) {
+      break;
+    }
+
+    if (-d $request_filename) {
+      break;
+    }
+
+    rewrite         ^(.+)$ /index.php?q=$1 last;
+
+    # redirect to feedburner.
+    # if ($http_user_agent !~ FeedBurner) {
+    #   rewrite ^/feed/?$ http://feeds.feedburner.com/feedburner-feed-id last;
+    # }
+  }
+
+  location ~ .*\.php$ {
+    include         /app/vendor/nginx/conf/fastcgi_params;
+    fastcgi_pass    unix:/tmp/php-fpm.socket;
+    fastcgi_param   SCRIPT_FILENAME $document_root$fastcgi_script_name;
+  }
 
 # for people with app root as doc root, restrict access to a few things
 location ~ ^/(composer\.|Procfile$|<?=getenv('COMPOSER_VENDOR_DIR')?>/|<?=getenv('COMPOSER_BIN_DIR')?>/) {
     deny all;
 }
 
-# WordPress multisite subdirectory rules.
-# Designed to be included in any server {} block.
-
-# This order might seem weird - this is attempted to match last if rules below fail.
-# http://wiki.nginx.org/HttpCoreModule
-location / {
-  try_files $uri $uri/ /index.php?$args;
-}
-
-# Directives to send expires headers and turn off 404 error logging.
-location ~* \.(js|css|png|jpg|jpeg|gif|ico)$ {
-  expires 24h;
-  log_not_found off;
-}
-
-location ~ ^/[_0-9a-zA-Z-]+/files/(.*)$ {
-        try_files /wp-content/blogs.dir/$blogid/files/$2 /wp-includes/ms-files.php?file=$2 ;
-        access_log off; log_not_found off; expires max;
-}
-
-# Uncomment one of the lines below for the appropriate caching plugin (if used).
-#include global/wordpress-ms-subdir-wp-super-cache.conf;
-#include global/wordpress-ms-subdir-w3-total-cache.conf;
-
-# Rewrite multisite '.../wp-.*' and '.../*.php'.
-if (!-e $request_filename) {
-  rewrite /wp-admin$ $scheme://$host$uri/ permanent;
-  rewrite ^/[_0-9a-zA-Z-]+(/wp-.*) $1 last;
-  rewrite ^/[_0-9a-zA-Z-]+(/.*\.php)$ $1 last;
-}
