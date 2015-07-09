90bdd5e772af24cc6e5690491bb29f0a8293c27d
Kevin Fling <k@livebreathedigital.net>
2015-07-09 02:35:28 -0400

    ahhhhh

--- conf/nginx/default_include.conf.php
+++ conf/nginx/default_include.conf.php
@@ -1,47 +1,40 @@
+# WordPress multisite subdirectory rules.
+# Designed to be included in any server {} block.
+
+# This order might seem weird - this is attempted to match last if rules below fail.
+# http://wiki.nginx.org/HttpCoreModule
 location / {
   try_files $uri $uri/ /index.php?$args;
 }
 
-location ~ \.php$ {
-  try_files $uri =404;
-  include fastcgi_params;
-  fastcgi_pass php;
-}
-
-# for people with app root as doc root, restrict access to a few things
-location ~ ^/(composer\.|Procfile$|<?=getenv('COMPOSER_VENDOR_DIR')?>/|<?=getenv('COMPOSER_BIN_DIR')?>/) {
-    deny all;
-}
-
-# Add trailing slash to */wp-admin requests.
-rewrite /wp-admin$ $scheme://$host$uri/ permanent;
-
-location ~ ^/files/(.*)$ {
-  try_files /wp-content/blogs.dir/$blogid/$uri /wp-includes/ms-files.php?file=$1 ;
-  access_log off; log_not_found off; expires max;
-}
-
+# Directives to send expires headers and turn off 404 error logging.
 location ~* \.(js|css|png|jpg|jpeg|gif|ico)$ {
   expires 24h;
   log_not_found off;
 }
 
-location ^~ /blogs.dir {
-  internal;
-  alias /var/www/pathtoyoursite/web/wp-content/blogs.dir ;
-  access_log off; log_not_found off;      expires max;
+location ~ ^/[_0-9a-zA-Z-]+/files/(.*)$ {
+        try_files /wp-content/blogs.dir/$blogid/files/$2 /wp-includes/ms-files.php?file=$2 ;
+        access_log off; log_not_found off; expires max;
 }
 
+#avoid php readfile()
+location ^~ /blogs.dir {
+        internal;
+        alias /var/www/example.com/htdocs/wp-content/blogs.dir ;
+        access_log off; log_not_found off;      expires max;
+}
+
+# Uncomment one of the lines below for the appropriate caching plugin (if used).
+#include global/wordpress-ms-subdir-wp-super-cache.conf;
+#include global/wordpress-ms-subdir-w3-total-cache.conf;
+
+# Rewrite multisite '.../wp-.*' and '.../*.php'.
 if (!-e $request_filename) {
   rewrite /wp-admin$ $scheme://$host$uri/ permanent;
   rewrite ^/[_0-9a-zA-Z-]+(/wp-.*) $1 last;
   rewrite ^/[_0-9a-zA-Z-]+(/.*\.php)$ $1 last;
 }
-
-# Directives to send expires headers and turn off 404 error logging.
-#location ~* ^.+\.(ogg|ogv|svg|svgz|eot|otf|woff|mp4|ttf|rss|atom|jpg|jpeg|gif|png|ico|zip|tgz|gz|rar|bz2|doc|xls|exe|ppt|tar|mid|midi|wav|bmp|rtf)$ {
-#  access_log off; log_not_found off; expires max;
-#}
 
 # Pass all .php files onto a php-fpm/php-fcgi server.
 location ~ \.php$ {
@@ -60,3 +53,8 @@ location ~ \.php$ {
 # fastcgi_intercept_errors on;
   fastcgi_pass php;
 }
+
+# for people with app root as doc root, restrict access to a few things
+#location ~ ^/(composer\.|Procfile$|<?=getenv('COMPOSER_VENDOR_DIR')?>/|<?=getenv('COMPOSER_BIN_DIR')?>/) {
+#    deny all;
+#}
